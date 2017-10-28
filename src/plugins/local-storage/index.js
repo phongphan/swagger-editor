665ee28e7fe7a7f75bc0dcdbe1326468df5586c3
phongphan <phongphan.p@gmail.com>
2017-10-29 01:59:46 +0700

    Add backendUrl supported

--- src/plugins/local-storage/index.js
+++ src/plugins/local-storage/index.js
@@ -1,5 +1,6 @@
 import PetstoreYaml from "./petstore"
 const CONTENT_KEY = "swagger-editor-content"
+const BACKEND_URL_KEY = "swagger-backend-url"
 
 let localStorage = window.localStorage
 
@@ -12,21 +13,35 @@ export const updateSpec = (ori) => (...args) => {
 export default function(system) {
   // setTimeout runs on the next tick
   setTimeout(() => {
-    if(localStorage.getItem(CONTENT_KEY)) {
-      system.specActions.updateSpec(localStorage.getItem(CONTENT_KEY))
-    } else if(localStorage.getItem("ngStorage-SwaggerEditorCache")) {
-      // Legacy migration for swagger-editor 2.x
-      try {
-        let obj = JSON.parse(localStorage.getItem("ngStorage-SwaggerEditorCache"))
-        let yaml = obj.yaml
-        system.specActions.updateSpec(yaml)
-        saveContentToStorage(yaml)
-        localStorage.setItem("ngStorage-SwaggerEditorCache", null)
-      } catch(e) {
+    var backendUrl = localStorage.getItem(BACKEND_URL_KEY)
+    if (backendUrl != null) {
+      var xhr = new XMLHttpRequest()
+      xhr.open('GET', backendUrl, false)
+      xhr.send()
+      if (xhr.status != 200) {
+        console.log(xhr.status + ': ' + xhr.statusText)
+      }
+      else {
+        system.specActions.updateSpec(xhr.responseText)
+      }
+    }
+    else {
+      if(localStorage.getItem(CONTENT_KEY)) {
+        system.specActions.updateSpec(localStorage.getItem(CONTENT_KEY))
+      } else if(localStorage.getItem("ngStorage-SwaggerEditorCache")) {
+        // Legacy migration for swagger-editor 2.x
+        try {
+          let obj = JSON.parse(localStorage.getItem("ngStorage-SwaggerEditorCache"))
+          let yaml = obj.yaml
+          system.specActions.updateSpec(yaml)
+          saveContentToStorage(yaml)
+          localStorage.setItem("ngStorage-SwaggerEditorCache", null)
+        } catch(e) {
+          system.specActions.updateSpec(PetstoreYaml)
+        }
+      } else {
         system.specActions.updateSpec(PetstoreYaml)
       }
-    } else {
-      system.specActions.updateSpec(PetstoreYaml)
     }
   }, 0)
   return {
@@ -41,5 +56,18 @@ export default function(system) {
 }
 
 function saveContentToStorage(str) {
-  return localStorage.setItem(CONTENT_KEY, str)
+  var backendUrl = localStorage.getItem(BACKEND_URL_KEY)
+  if (backendUrl != null) {
+    var xhr = new XMLHttpRequest()
+    xhr.open('PUT', backendUrl, true)
+    xhr.send(str)
+
+    if (xhr.status != 200) {
+      console.log(xhr.status + ': ' + xhr.statusText)
+    }
+    return str
+  }
+  else {
+    return localStorage.setItem(CONTENT_KEY, str)
+  }
 }
